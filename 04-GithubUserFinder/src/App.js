d5ac5e7d9086e0725b59820abded98a4feb46ed9
amang <amangupta6121998@gmail.com>
2021-08-19 14:27:46 +0530

    added : state for handling user Input

--- 04-GithubUserFinder/src/App.js
+++ 04-GithubUserFinder/src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./App.css";
 import Addcard from "./components/Addcard";
+import Usercard from "./components/Usercard";
 
 class App extends React.Component {
+  state = {
+    userInput: "",
+  };
+
+  getUserInput = (val) => {
+    const userInput = val;
+
+    if (userInput !== "") {
+      this.setState({
+        userInput,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <h1>The Github User Finder</h1>
-        <Addcard />
+        <Addcard updateUserInput={this.getUserInput} />
+        <Usercard searchUser={this.state.userInput} />
       </div>
     );
   }
