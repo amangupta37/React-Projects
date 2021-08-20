ceb4d0fa2b6a7a35d8c96e439c54549dc6468961
amang <amangupta6121998@gmail.com>
2021-08-20 08:28:17 +0530

    added : API call using Axios

--- 04-GithubUserFinder/src/components/Addcard.js
+++ 04-GithubUserFinder/src/components/Addcard.js
@@ -1,33 +1,44 @@
 import React from "react";
+import axios from "axios";
 
 class Addcard extends React.Component {
   state = {
     name: "",
   };
+
+  handleSubmit = async (userInput) => {
+    const response = await axios.get(
+      `https://api.github.com/users/${userInput}`
+    );
+    const data = response.data;
+
+    this.props.updateUserInput(data);
+  };
+
+  takeUserInput = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  addUser = () => {
+    this.handleSubmit(this.state.name);
+    this.setState({
+      name: "",
+    });
+  };
+
   render() {
-    const takeUserInput = (e) => {
-      this.setState({
-        name: e.target.value,
-      });
-    };
-
-    const addUser = () => {
-      this.props.updateUserInput(this.state.name);
-      this.setState({
-        name: "",
-      });
-    };
-
     return (
       <div className="Addcard">
         <div className="search">
           <input
             type="text"
             placeholder="Github username"
-            onChange={takeUserInput}
+            onChange={this.takeUserInput}
             value={this.state.name}
           />
-          <button onClick={addUser} disabled={!this.state.name}>
+          <button onClick={this.addUser} disabled={!this.state.name}>
             Add Card
           </button>
         </div>
