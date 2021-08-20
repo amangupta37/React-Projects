2555dada5d85a31447ece42e2f1e6fc12cd023f5
amang <amangupta6121998@gmail.com>
2021-08-20 12:01:45 +0530

    fix : display dynamic data

--- 04-GithubUserFinder/src/components/Usercard.js
+++ 04-GithubUserFinder/src/components/Usercard.js
@@ -1,20 +1,35 @@
 import React from "react";
-
 class Usercard extends React.Component {
   render() {
     console.log(this.props.searchUser);
+
     return (
-      <div className="UserCard">
-        <div className="userProfile">
-          <div className="userImage">
-            <img src="https://randomuser.me/api/portraits/women/5.jpg" alt="" />
-          </div>
-          <div className="userName">
-            <h2>User Name</h2>
-            <h4>@username</h4>
-          </div>
-        </div>
-      </div>
+      <>
+        {this.props.searchUser
+          .slice(0)
+          .reverse()
+          .map((resp) => {
+            return (
+              <div className="UserCard" key={resp.id}>
+                <a href={resp.html_url} target="_blank" rel="noreferrer">
+                  <div className="userProfile">
+                    <div className="userImage">
+                      <img src={resp.avatar_url} alt="sss" />
+                    </div>
+                    <div className="userName">
+                      {resp.name !== null ? (
+                        <h2>{resp.name}</h2>
+                      ) : (
+                        <h2>UserName</h2>
+                      )}
+                      <p>@{resp.login}</p>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            );
+          })}
+      </>
     );
   }
 }
