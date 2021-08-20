1d1dc8ce7f0fafae32c69dcc329558a432319f23
amang <amangupta6121998@gmail.com>
2021-08-20 12:00:32 +0530

    created : header components

---
+++ 04-GithubUserFinder/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="logo">
+          <img
+            src="https://img.icons8.com/fluency-systems-filled/144/ffffff/github.png"
+            alt=""
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
