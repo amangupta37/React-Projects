e219717dcf34c8599726264cea65652b2be4cc38
amang <amangupta6121998@gmail.com>
2021-08-22 20:39:22 +0530

    added : components

--- 05-StarMatch/src/App.js
+++ 05-StarMatch/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import "./App.css";
+import { Keypad } from "./components/Keypad";
+import Star from "./components/Star";
 
 const App = () => {
   return (
     <div className="App">
       <h1>hello react</h1>
+      <Star />
+      <Keypad />
     </div>
   );
 };
